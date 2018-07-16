533bbad07b8b7257478a31ce4fae70c8e6f3a6b7
Leah Enelow <lenelow@students.terpmail.edu>
2018-07-16 11:28:00 -0400

    .

--- App.js
+++ App.js
@@ -1,37 +1,11 @@
-import React from "react";
-<<<<<<< HEAD
-import { AppRegistry, StyleSheet, Image, Text, View } from "react-native";
-import Image from "./components/Image";
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View>
-        <Text>Hello World</Text>
-        <Image
-          style={styles.image}
-          source={{
-            uri: "https://wallpaperbrowse.com/media/images/750814.jpg"
-          }}
-        />
-      </View>
-    );
-=======
+import React from "../../../Library/Caches/typescript/2.9/node_modules/@types/react";
 import { StyleSheet, Text, View } from "react-native";
-import Picture from "./components/Image";
+import Picture from "./components/Picture";
 
 export default class App extends React.Component {
   render() {
     return <Picture />;
->>>>>>> b1e12c69247f6d7c2e08dd5731eabda03f526755
   }
 }
-
-const styles = StyleSheet.create({
-  image: {
-    width: 200,
-    height: 200
-  }
-});
 
 AppRegistry.registerComponent("App", () => App);
--- components/Picture.js
+++ components/Picture.js
@@ -8,7 +8,16 @@ export default class Picture extends Component {
     //     "https://images.unsplash.com/photo-1531674590200-f5e4c43f31de?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=44c88230f10a563c9dd67d6456dc1c35&auto=format&fit=crop&w=2850&q=80"
 
     return (
-      <Image source={require("../photo-1531674590200-f5e4c43f31de.jpeg")} />
+      <Image
+        style={styles.image}
+        source={require("../photo-1531674590200-f5e4c43f31de.jpeg")}
+      />
     );
   }
 }
+const styles = StyleSheet.create({
+  image: {
+    width: 50,
+    height: 50
+  }
+});
