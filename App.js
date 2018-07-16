13e53fa155d038e1a4a7f823ad38b3ff7100d1b1
Mariya <mariyawhitcomb@gmail.com>
2018-07-16 10:44:42 -0400

    added image to app

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import Image from "./components/Image";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Hello World</Text>
-        {/* <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text> */}
+        <Image />
       </View>
     );
   }
